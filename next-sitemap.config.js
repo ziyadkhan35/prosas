8e134735f52c5a42f3c759840e3e2d76997b8006
ziyadkhan <murudovz17@gmail.com>
2024-11-18 14:32:40 +0400

    sitemap issues try to fixed

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,10 +1,14 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
     siteUrl: 'https://prosas.az',
-    generateRobotsTxt: true,      
-    sitemapSize: 7000,            
-    changefreq: 'week',          
-    priority: 0.7,               
-    exclude: ['/admin/*'],       
+    generateRobotsTxt: true,
+    sitemapSize: 7000,
+    changefreq: 'weekly',
+    priority: 0.7,
+    exclude: ['/admin/*'],
+    alternateRefs: [
+        { href: 'https://prosas.az/az', hreflang: 'az' },
+        { href: 'https://prosas.az/en', hreflang: 'en' },
+        { href: 'https://prosas.az/ru', hreflang: 'ru' },
+    ],
 };
-  
