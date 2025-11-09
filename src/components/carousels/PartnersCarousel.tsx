0ec0c1d306572c8dc2161c8e153f10a5963ed81c
ziyadkhan <murudovz17@gmail.com>
2025-11-09 23:10:46 +0400

    partners comp modified

--- src/components/carousels/PartnersCarousel.tsx
+++ src/components/carousels/PartnersCarousel.tsx
@@ -4,6 +4,18 @@ import useEmblaCarousel from 'embla-carousel-react'
 import Autoplay from 'embla-carousel-autoplay'
 import Image from 'next/image'
 
+const partnerImages = [
+  '/partners/bosch.png',
+  '/partners/ateksis.png',
+  '/partners/ahg.png',
+  '/partners/hikvision.png',
+  '/partners/abv.png',
+  '/partners/azerconnect.png',
+  '/partners/microsoft.png',
+  '/partners/komtes.png',
+  '/partners/honeywell.svg',
+]
+
 const PartnersCarousel = () => {
 
     const [emblaRef] = useEmblaCarousel({ loop: true }, [Autoplay({ delay: 1000, stopOnInteraction: false })])
@@ -11,27 +23,16 @@ const PartnersCarousel = () => {
   return (
     <div className="embla overflow-hidden px-2" ref={emblaRef}>
       <div className="embla__container flex">
-        <div className="embla__slide flex-none basis-1/3 sm:basis-1/6 mr-6 md:mr-8 min-w-0 relative flex items-center justify-center">
-          <Image src={'/partners/bosch.png'} width={150} height={150} alt='image of partner' />
-        </div>
-        <div className="embla__slide flex-none basis-1/3 sm:basis-1/6 mr-6 md:mr-8 min-w-0 relative flex items-center justify-center">
-          <Image src={'/partners/ateksis.png'} width={150} height={150} alt='image of partner' />
-        </div>
-        <div className="embla__slide flex-none basis-1/3 sm:basis-1/6 mr-6 md:mr-8 min-w-0 relative flex items-center justify-center">
-          <Image src={'/partners/ahg.png'} width={150} height={150} alt='image of partner' />
-        </div>
-        <div className="embla__slide flex-none basis-1/3 sm:basis-1/6 mr-6 md:mr-8 min-w-0 relative flex items-center justify-center">
-          <Image src={'/partners/hikvision.png'} width={150} height={150} alt='image of partner' />
-        </div>
-        <div className="embla__slide flex-none basis-1/3 sm:basis-1/6 mr-6 md:mr-8 min-w-0 relative flex items-center justify-center">
-          <Image src={'/partners/abv.png'} width={150} height={150} alt='image of partner' />
-        </div>
-        <div className="embla__slide flex-none basis-1/3 sm:basis-1/6 mr-6 md:mr-8 min-w-0 relative flex items-center justify-center">
-          <Image src={'/partners/azerconnect.png'} width={150} height={150} alt='image of partner' />
-        </div>
-        <div className="embla__slide flex-none basis-1/3 sm:basis-1/6 mr-6 md:mr-8 min-w-0 relative flex items-center justify-center">
-          <Image src={'/partners/microsoft.png'} width={150} height={150} alt='image of partner' />
-        </div>
+        {
+          partnerImages.map((src, index) => (
+            <div 
+              key={index}
+              className="embla__slide flex-none basis-1/3 sm:basis-1/6 mr-6 md:mr-8 min-w-0 relative flex items-center justify-center"
+            >
+              <Image src={src} width={150} height={150} alt={`partner-${index}`} />
+            </div>
+          ))
+        }
       </div>
     </div>
   )
