c06242aa579007a013f58b26d89a02796e3aab0b
Ziyadkhan <112168500+ziyadkhan35@users.noreply.github.com>
2026-01-03 19:31:54 +0400

    Update layout.tsx
    Google search condole

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -11,6 +11,9 @@ export const metadata: Metadata = {
   metadataBase: new URL('https://prosas.az'),
   title: "ProSAS",
   description: "ProSAS provides advanced fire protection systems, IT equipment installation, and security solutions to keep your assets safe.",
+  verification: {
+    google: "szwBREwThnn_z98-zovPUJG72PshlRNdfCNBNoDRXwo",
+  },
   keywords: ['fire protection', 'IT equipment installation', 'security systems', 'CCTV', 'emergency announcement', 'fm-200', 'ProSAS', 'prosas', 'yanğından mühafizə sistemləri','CCTV', 'kamera təhlükəsizliyi', 'video müşahidə','şəbəkə infrastruktur', 'qazlı yanğın söndürmə', ' ictimai anons sistemləri', 'fon musiqisi sistemləri', 'системы фоновой музыки', 'bina avtomatlaşdırılması', 'автоматизация зданий', 'системы оповещения в чрезвычайных ситуациях', 'газовое пожаротушение', 'сеть инфраструктура', 'системы сетей для бизнеса', 'IT решения для сетей', 'LAN', 'WAN', 'системы видеонаблюдения', 'камеры безопасности', 'системы пожарной защиты'],
   authors: [{ name: 'Ziyadkhan Murudov' }],
   creator: 'Ziyadkhan Murudov',
